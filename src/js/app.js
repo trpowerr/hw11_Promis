61314c0ca09ec5dc375815f3aa6d48bc8042f08c
trpowerr <mixazbubl@yandex.ru>
2020-02-24 20:49:36 +0300

    add test

--- src/js/app.js
+++ src/js/app.js
@@ -1,30 +1,6 @@
-function read() {
-  return new Promise((resolve, reject) => {
-    // эмуляция чтения файла
-    setTimeout(() => {
-      const data = '{"id":9,"created":1546300800,"userInfo":{"id":1,name":"Hitman","level":10,"points":2000}}';
-      return (input => {
-        const buffer = new ArrayBuffer(input.length * 2);
-        const bufferView = new Uint16Array(buffer);
-        for (let i = 0; i < input.length; i++) {
-          bufferView[i] = input.charCodeAt(i);
-        }
-        resolve(buffer);
-      })(data);
-    }, 1000); 
-  });
-}
+import { read } from './reader';
+import { json } from './parser';
 
-function json(data) {
-  return new Promise((resolve, reject) => {
-    // эмуляция обработки ArrayBuffer
-    setTimeout(() => {
-      resolve(String.fromCharCode.apply(null, new Uint16Array(data)));
-    }, 500);
-  });
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////
 
 class GameSavingLoader {
 
@@ -40,13 +16,4 @@ class GameSavingLoader {
   }
 }  
 
-const newSave = new GameSavingLoader();
-
-// console.log(newSave.load());
-
-
-newSave.load().then((saving) => {
-    console.log('Save ', saving)
-  }, (error) => {
-    console.log('ERORRR')
-  });
+ export const newSave = new GameSavingLoader();
